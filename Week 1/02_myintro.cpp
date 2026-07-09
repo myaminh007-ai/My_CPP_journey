04eec172b08501636b784986fafdc8cd68fc7dc4
MD. Yamin Hossain <myaminh007@gmail.com>
2026-07-09 14:13:04 +0600

    My introduction

---
+++ Week 1/02_myintro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "My name: Md. Yamin Hossain" << endl;
+    cout << "University: United International University" << endl;
+    cout << "Date of birth: 22-02-2006" << endl;
+    cout << "Email: myaminh007@gmail.com" << endl;
+
+    return 0;
+}
