7a7bc83a9f74ff251a88b3af5f966be2e41cd1c9
MD. Yamin Hossain <myaminh007@gmail.com>
2026-07-10 11:00:06 +0600

    Using comment in c++

---
+++ Week 1/04_comment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a,b;//here I used float for storing decimal
+    cout<<"Enter a =";
+    cin>>a;
+    cout<<"Enter b =";
+    cin>>b;
+    cout<<"a = "<<a<<", b = "<<b<<endl;
+
+    return 0;
+}
